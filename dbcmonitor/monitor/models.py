b372863f0fa987389a6e3fdbd3487671c7e3a973
Alexandre Barbosa <alexandreab@live.com>
2015-03-27 15:47:00 -0300

    Models for Replications comparison
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>

--- dbcmonitor/monitor/models.py
+++ dbcmonitor/monitor/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Replication(models.Model):
+    host_name = models.CharField(max_length=15)
+    rep_user = models.CharField(max_length=30)
+    log_file = models.CharField(max_length=50)
+    log_position = models.IntegerField()
+
+class SlaveReplication(Database):
+    master_rep = models.ForeignKey('Replication')
+
+class Comparison(models.Model):
+    master_rep = models.ForeignKey('Replication')
+    slave_rep = models.ForeignKey('SlaveReplication')
+
+
+class DatabaseStatus(models.Model):
+    replication = models.ForeignKey('Replication')
+    db_name = models.CharField(max_length=20)
+
+class TableStatus(models.Model):
+    database = models.ForeignKey('Database')
+    table_name = models.CharField(max_length=20)
+    status = models.CharField(max_length=15)
