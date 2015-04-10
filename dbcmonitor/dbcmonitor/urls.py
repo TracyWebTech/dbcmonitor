43bbd6ff222abf79c86046f2ac738b26ddf74c24
Alexandre Barbosa <alexandreab@live.com>
2015-04-10 15:42:11 -0300

    Handle templates files to extend from root
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>

--- dbcmonitor/dbcmonitor/urls.py
+++ dbcmonitor/dbcmonitor/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns(
     '',
+
+    url(r'^$', RedirectView.as_view(url='/monitor')),
 
     url(r'^monitor/', include('monitor.urls')),
 
