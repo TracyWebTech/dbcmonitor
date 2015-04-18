ebe881ea75a180d25da35336ff55fb8b644df706
Sergio Oliveira <sergio@tracy.com.br>
2015-04-18 10:18:45 -0300

    Moved organization to it's own app

--- dbcmonitor/monitor/models.py
+++ dbcmonitor/monitor/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class Organization(models.Model):
-    name = models.CharField(max_length=30)
-    token = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.name
-
-
 class Replication(models.Model):
     master_rep = models.ForeignKey('self',
                                    related_name='master',
